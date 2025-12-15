01415720aa1acd52ef1c1e8e87382d995e38d88e
saintreborn <richardsonreuben78@gmail.com>
2025-12-15 15:22:35 +0300

    seo set up

--- Public-Site/src/index.tsx
+++ Public-Site/src/index.tsx
@@ -1,12 +1,5 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { App } from './App';
-import { Analytics } from '@vercel/analytics/react'; // import Analytics
-
-render(
-    <>
-        <App />
-        <Analytics />
-    </>,
-    document.getElementById('root')
-);
+import './index.css';
+import React from "react";
+import { render } from "react-dom";
+import { App } from "./App";
+render(<App />, document.getElementById("root"));
